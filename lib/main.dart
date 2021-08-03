80872f8462f1e9e51f8a1b25ea32eb2c6590c81d
LariSolino <solinolarissa@gmail.com>
2021-08-03 20:50:44 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+// ignore: use_key_in_widget_constructors
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/main.dart';
+// ignore: unused_import
+import 'package:payflow/shared/themes/app_colors.dart';
+
+// ignore: unused_import
+import 'app_widget.dart';
+import 'modules/login/login_page.dart';
+
+void main() {
+  runApp(AppFirebase());
+}
+
+class AppFirebase extends StatefulWidget {
+  @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Material(
+              child: Center(
+                child: Text(
+                  "Não foi posível inicializar o Firebase",
+                  textDirection: TextDirection.ltr,
+                ),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return AppWidget();
+          } else {
+            return Material(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
+  }
+}
